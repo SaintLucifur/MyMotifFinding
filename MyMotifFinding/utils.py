908a670b90d92b9a72b29121971378d2debfa858
SaintLucifur <108704858+SaintLucifur@users.noreply.github.com>
2023-05-20 02:04:22 -0700

    Update utils.py

--- MyMotifFinding/utils.py
+++ MyMotifFinding/utils.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import math
+import nbformat
 
 fileName = ".\Test\peaksTest.txt"
 
@@ -33,27 +34,33 @@ def getPeaksDict(fileName):
     peaksDict.pop("#PeakID", None)            
     return peaksDict
 
+
 def getPFM(sequencesDict):
-    for key in peaksDict.keys():
-        sequence = peaksDict[key][4]
-        
-def getPFM(sequences):
     nucs = {"A": 0, "C": 1, "G": 2, "T": 3}
-    pfm = np.zeros((4, len(sequences[0])))
+    pfm = np.zeros((4, len(list(sequencesDict.keys())[0])))
     
-    for i in range (len(sequences)):
-        for j in range(len(sequences[0])):
-            pfm[nucs[sequences[i][j]]][j] += 1
+    for sequence in sequencesDict.keys():
+        for j in range(len(sequence)):
+            pfm[nucs[sequence[j]]][j] += sequencesDict[sequence]
+        
     return pfm
+
+# def getPFM(sequences):
+#     nucs = {"A": 0, "C": 1, "G": 2, "T": 3}
+#     pfm = np.zeros((4, len(sequences[0])))
+    
+#     for i in range (len(sequences)):
+#         for j in range(len(sequences[0])):
+#             pfm[nucs[sequences[i][j]]][j] += 1
+#     return pfm
 
 def getBackgroundFreq():
     background_freq = [0.25, 0.25, 0.25, 0.25]
     return background_freq
 
-def getPWM(sequences, background_freqs=[0.25, 0.25, 0.25, 0.25]):
+def getPWM(pfm, background_freqs=[0.25, 0.25, 0.25, 0.25]):
     
-    pwm = np.zeros((4, len(sequences[0])))
-    pfm = getPFM(sequences)
+    pwm = np.zeros((4, len(pfm[0])))
     pfm = pfm + 0.01
     
     for i in range(len(pfm)):
@@ -69,5 +76,36 @@ def main():
         if key == '17-14':
             print(dict[key][1], dict[key][2])
     
+    example = {
+        "ATTAG":5,
+        "TCGCG":6,
+        "GGTGG":7,
+        "TATAG":8,
+        "TACGG":4,
+        "AGCCG":3
+    }
+    pfm = getPFM(example)
+    print(pfm)
+    pwm = getPWM(pfm)
+    print(pwm)
+    
+    seqlogoCode = """
+import seqlogo
+# make seqlogo PWM object
+seq_pwm = seqlogo.Pwm(pwm)
+# Convert to ppm needed for plotting
+seq_ppm = seqlogo.Ppm(seqlogo.pwm2ppm(seq_pwm))
+seqlogo.seqlogo(seq_ppm, ic_scale = True, format = 'png', size = 'medium')
+    """
+    
+    jupy = nbformat.v4.new_notebook()
+    pwmString = """pwm = """ + str(repr(np.array(pwm))) + '\n' + seqlogoCode
+    print(pwmString)
+    
+    jupy['cells'] = [nbformat.v4.new_code_cell(pwmString)]
+    nbformat.write(jupy,'Test.ipynb')
+    
+    # jupy['cells'] = [nbformat.v4.new_code_cell(seqlogoCode)]
+    # nbformat.write(jupy,'Test.ipynb')
     
 main()
