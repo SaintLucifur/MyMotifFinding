8fea6fe114bc231d8672097a00dd7cb401f5f504
A-P-Q <114311404+A-P-Q@users.noreply.github.com>
2023-05-24 03:26:43 -0700

    Update MyMotifFinding.py
    The dict.update() method updates the dictionary in-place and returns None. So, return(dict2.update(dict1)) will always return None. I fixed the issue by changing the code to merged_dict = dict1.copy()
        merged_dict.update(dict2)
        return merged_dict

--- MyMotifFinding/MyMotifFinding.py
+++ MyMotifFinding/MyMotifFinding.py
@@ -12,8 +12,10 @@ import sys
 import loadGenome
 
 def mergeDict(dict1, dict2):
-    return(dict2.update(dict1))
-
+    merged_dict = dict1.copy()
+    merged_dict.update(dict2)
+    return merged_dict
+    
 def main():
     parser = argparse.ArgumentParser(
         prog="mmf",
